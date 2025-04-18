6044c65261a54b486024d2dabdd7b98dfdf530cd
krishna ramoliya <ramoliyakrishna858@gmail.com>
2025-04-18 17:15:14 +0530

    hello

---
+++ nest 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=1,b=10,c=5;
+	if(a>b)
+	
+		if(c>b)
+	
+	printf("b is min");
+	else{
+	printf("c is min");}
+	else{
+		if(c>a){
+			printf("a is min");
+		}else printf("c is main");
+		
+	}
+}
