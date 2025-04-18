5c7fdd19acd62b5c7ea50e9a15990bb88aa1febe
krishna ramoliya <ramoliyakrishna858@gmail.com>
2025-04-18 16:45:40 +0530

    project work

---
+++ triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int first,second,third;
+	
+	printf("enter value first is:");
+	scanf("%d",&first);
+	
+	printf("enter value second is:");
+	scanf("%d",&second);
+	
+	third=180-first-second;
+	printf("%d",third);
+	
+}
