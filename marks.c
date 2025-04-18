8fb215f32f4d7e4462c94fee76f0e8eef652c302
krishna ramoliya <ramoliyakrishna858@gmail.com>
2025-04-18 16:49:24 +0530

    hello

---
+++ marks.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int maths,eng,acc,total;
+	float per
+	
+	
+	printf("enter value maths:");
+	scanf("%d",&maths);
+	
+	printf("enter value eng:");
+	scanf("%d",&eng);
+	
+	printf("enter value acc:");
+	scanf("%d",&acc);
+	
+	
+	total=maths+eng+acc;
+	printf("%d",total);
+	
+	per=(total*100)/300;
+	printf("%d",per);
+}
