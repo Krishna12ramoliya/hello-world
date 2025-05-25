624139c84df042ca76644adf8d43f62ff2dcc8ca
krishna ramoliya <ramoliyakrishna858@gmail.com>
2025-05-25 22:01:16 +0530

    exam

---
+++ EXAM 4.cpp
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+void squareArray(int *arr, int n)
+ {
+    for (int i = 0; i < 5; i++)
+	 {
+        
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    i
+    squareArray(arr[i]);
+
+    printf("Square array elements:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]*arr[i]);
+    }
+    printf("\n");
+
+   
+}
+/*
+Square array elements:
+1 4 9 16 25
+
+*/
+
+
